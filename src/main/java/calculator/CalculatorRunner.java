76548c8f908e77e664a0fada4b591f695ed197f1
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-17 13:57:05 +0900

    feat: 실행 로직 구현

--- src/main/java/calculator/CalculatorRunner.java
+++ src/main/java/calculator/CalculatorRunner.java
@@ -1,5 +1,13 @@
 package calculator;
 
-public class CalculatorRunner {
+import calculator.controller.CalculatorController;
+import calculator.service.CalculatorService;
 
+public class CalculatorRunner {
+	public void run() {
+		CalculatorService calculatorService = new CalculatorService();
+		CalculatorController calculatorController = new CalculatorController(calculatorService);
+
+		calculatorController.start();
+	}
 }

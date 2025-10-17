32089e9bd02a6e479f1d8d680173ec7306145a66
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-17 14:35:31 +0900

    feat: 구분자 활용한 합계 기능 구현

--- src/main/java/calculator/service/CalculatorService.java
+++ src/main/java/calculator/service/CalculatorService.java
@@ -1,4 +1,29 @@
 package calculator.service;
 
+import java.util.Arrays;
+
+import calculator.domain.Delimiter;
+
 public class CalculatorService {
+
+	public int calculate(String text) {
+		if (text == null || text.trim().isEmpty()) {
+			return 0;
+		}
+		Delimiter delimiter = Delimiter.fromText(text);
+		String[] token = delimiter.getNumberText().split(delimiter.getDelimiter());
+
+		if (token.length == 0 || token.length == 1 && token[0].isEmpty()) {
+			return 0;
+		}
+		return Arrays.stream(token)
+			.mapToInt(this::parseToInt)
+			.sum();
+
+	}
+
+	private int parseToInt(String numberText) { // 숫자 변환
+		return Integer.parseInt(numberText);
+	}
+
 }

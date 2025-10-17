9e60233eea345ee9b24c670ca9df3e1a2de0243f
rhkrqkq <rhkrqkq1025@gmail.com>
2025-10-17 14:02:08 +0900

    feat: 입력받기 구현

--- src/main/java/calculator/view/InputView.java
+++ src/main/java/calculator/view/InputView.java
@@ -1,4 +1,16 @@
 package calculator.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+
+	public static String readInput() {
+		System.out.println("덧셈할 문자열을 입력해주세요");
+		String inputString = Console.readLine();
+
+		if (inputString == null || inputString.trim().isEmpty()) {
+			throw new IllegalArgumentException("입력값이 비어있습니다.");
+		}
+		return inputString;
+	}
 }
